74461e97ba4515d62605122195ec28d7db69ed68
Yasya12 <jasja200411@gmail.com>
2023-09-01 17:57:27 +0300

    make code looks better

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,124 +24,120 @@ namespace Rock_Paper_Scissor
     public partial class MainWindow : Window
     {
         DispatcherTimer gameTimer = new DispatcherTimer();
+
         ImageBrush robotIcon = new ImageBrush();
         ImageBrush rockIcon = new ImageBrush();
         ImageBrush paperIcon = new ImageBrush();
         ImageBrush scissorIcon = new ImageBrush();
 
-        ImageBrush robotChoice = new ImageBrush();
-
         Random random = new Random();
 
-        string robotResult, playerResult;
+        string robotResult, playerResult; 
 
-        int playerPoint = 0, robotPoint = 0, drawPoint = 0;
+        int playerPoint = 0, robotPoint = 0, drawPoint = 0; 
+
+        Color color = (Color)ColorConverter.ConvertFromString("#FAF1E4");
+
         public MainWindow()
         {
             InitializeComponent();
 
+            InitializeUIElementsWithImages();
+
             gameTimer.Start();
             gameTimer.Tick += StartGame;
-            gameTimer.Interval = TimeSpan.FromSeconds(3);
-
-            /*robotIcon.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/robot.png"));
-            robot.Fill = robotIcon;
-
-            rockIcon.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/rock.png"));
-            rock.Fill = rockIcon;
-
-            paperIcon.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/paper.png"));
-            paper.Fill = paperIcon;
-
-            scissorIcon.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/scissor.png"));
-            scissor.Fill = scissorIcon;
-
-            GenerateRobotChoice();*/
-            // StartGame();
-            /*gameTimer.Start();
-            gameTimer.Tick += StartGame;
-            gameTimer.Interval = TimeSpan.FromSeconds(1);*/
+            gameTimer.Interval = TimeSpan.FromSeconds(2);
         }
 
+        //тут встановлюються картинки
+        private void InitializeUIElementsWithImages()
+        {
+            SetElementBackgroundImage(ref robotIcon, robot, "pack://application:,,,/images/robot.png");
+            SetElementBackgroundImage(ref rockIcon, rock, "pack://application:,,,/images/rock.png");
+            SetElementBackgroundImage(ref paperIcon, paper, "pack://application:,,,/images/paper.png");
+            SetElementBackgroundImage(ref scissorIcon, scissor, "pack://application:,,,/images/scissor.png");
+        }
+        private void SetElementBackgroundImage(ref ImageBrush icon, UIElement element, string imagePath)
+        {
+            icon.ImageSource = new BitmapImage(new Uri(imagePath, UriKind.RelativeOrAbsolute));
+            (element as Shape).Fill = icon;
+        }
+        
+        //тут починається гра
         private void StartGame(object sender, EventArgs e)
         {
-            Color color = (Color)ColorConverter.ConvertFromString("#FAF1E4");
+            color = (Color)ColorConverter.ConvertFromString("#FAF1E4");
             BaseCanvas.Background = new SolidColorBrush(color);
+
             BaseCanvas.Visibility = Visibility.Visible;
             ChoseCanvas.Visibility = Visibility.Hidden;
 
-            robotIcon.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/robot.png"));
-            robot.Fill = robotIcon;
-
-            rockIcon.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/rock.png"));
-            rock.Fill = rockIcon;
-
-            paperIcon.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/paper.png"));
-            paper.Fill = paperIcon;
-
-            scissorIcon.ImageSource = new BitmapImage(new Uri("pack://application:,,,/images/scissor.png"));
-            scissor.Fill = scissorIcon;
-            
             GenerateRobotChoice();
-
         }
-
+        
+        //тут генерується вибір робота
         private void GenerateRobotChoice()
         {
             int r;
-            string path = "";
-
             r = random.Next(0, 3);
 
             switch (r) 
             {
                 case 0:
-                    path = "pack://application:,,,/images/rock.png";
+                    robotChose.Fill = rockIcon;
                     robotResult = "rock";
                     break;
                 case 1:
-                    path = "pack://application:,,,/images/paper.png";
+                    robotChose.Fill = paperIcon;
                     robotResult = "paper";
                     break;
                 case 2:
-                    path = "pack://application:,,,/images/scissor.png";
+                    robotChose.Fill = scissorIcon;
                     robotResult = "scissor";
                     break;
             }
-
-            robotChoice.ImageSource = new BitmapImage(new Uri(path));
-            robotChose.Fill = robotChoice;
         }
 
-        private void GenerateResult()
+        //тут оюробник подій і відповідно вибір користувача встановлюється
+        //у мене є подія MouseLeftButtonDown і коли вона виконується викликається метод\обробник подій і я можу визнатичити , що саме має відбуватися, коли виникає ця подія, в середині методу Choice_MouseLeftButtonDown. 
+        private void Choice_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            //rock > scissor
-            //scissor > paper
-            //paper > rock            
-            Dictionary<string, int> resultPairs = new Dictionary<string, int>();
+            Rectangle choice = (Rectangle)sender;
 
-            resultPairs["rock-rock"] = 0;
-            resultPairs["paper-paper"] = 0;
-            resultPairs["scissor-scissor"] = 0;
-            resultPairs["rock-scissor"] = 1;
-            resultPairs["paper-rock"] = 1;
-            resultPairs["scissor-paper"] = 1;
-            resultPairs["rock-paper"] = 2;
-            resultPairs["paper-scissor"] = 2;
-            resultPairs["scissor-rock"] = 2;
-
-            Color color = (Color)ColorConverter.ConvertFromString("#435334"); 
-
-            string key = robotResult + '-' + playerResult;
-            int val = resultPairs[key];
-
-            if(val == 1)
+            if (choice == rock)
             {
-                result.Content = "Lose!";
-                color = (Color)ColorConverter.ConvertFromString("#435334");
-                robotScore.Content = "robot:  " + ++robotPoint;
+                playerChose.Fill = rockIcon;
+                playerResult = "rock";
             }
-            else if (val == 2)
+            else if (choice == paper)
+            {
+                playerChose.Fill = paperIcon;
+                playerResult = "paper";
+            }
+            else if (choice == scissor)
+            {
+                playerChose.Fill = scissorIcon;
+                playerResult = "scissor";
+            }
+
+            ChoseCanvas.Visibility = Visibility.Visible;
+            CalculateResult();
+        }
+
+        //тут обраховується результат
+        private void CalculateResult()
+        {
+            if (playerResult == robotResult)
+            {
+                result.Content = "Draw!";
+                color = (Color)ColorConverter.ConvertFromString("#9EB384");
+                drawScore.Content = "draw:  " + ++drawPoint;
+            }
+            else if (
+                (playerResult == "rock" && robotResult == "scissor") ||
+                (playerResult == "paper" && robotResult == "rock") ||
+                (playerResult == "scissor" && robotResult == "paper")
+            )
             {
                 result.Content = "Win!";
                 color = (Color)ColorConverter.ConvertFromString("#CEDEBD");
@@ -149,37 +145,13 @@ namespace Rock_Paper_Scissor
             }
             else
             {
-                result.Content = "Draw!";
-                color = (Color)ColorConverter.ConvertFromString("#9EB384");
-                drawScore.Content = "draw:  " + ++drawPoint;
+                result.Content = "Lose!";
+                color = (Color)ColorConverter.ConvertFromString("#435334");
+                robotScore.Content = "robot:  " + ++robotPoint;
             }
 
             ChoseCanvas.Background = new SolidColorBrush(color);
             BaseCanvas.Background = new SolidColorBrush(color);
-        }
-
-        private void rock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            playerChose.Fill = rockIcon;
-            playerResult = "rock";
-            ChoseCanvas.Visibility = Visibility.Visible;
-            GenerateResult();
-        }
-
-        private void paper_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            playerChose.Fill = paperIcon;
-            playerResult = "paper";
-            ChoseCanvas.Visibility = Visibility.Visible;
-            GenerateResult();
-        }
-
-        private void scissor_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            playerChose.Fill = scissorIcon;
-            playerResult = "scissor";
-            ChoseCanvas.Visibility = Visibility.Visible;
-            GenerateResult();
         }
     }
 }
